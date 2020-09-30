b8198e8f924b80552d1b7683c47d6d39dccb3a21
Tobias Bardino <tobias@bardino.dk>
2020-09-30 09:42:29 +0200

    Add BaseStack

--- basestack/lib/base-stack.ts
+++ basestack/lib/base-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 
-export class BasestackStack extends cdk.Stack {
+export class BaseStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
