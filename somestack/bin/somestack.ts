81c7746f6146be8b6feb27a5f96709dc273284bb
Tobias Bardino <tobias@bardino.dk>
2020-09-30 11:04:19 +0200

    Align files and add SomeStact to App

--- somestack/bin/somestack.ts
+++ somestack/bin/somestack.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import {SomeLibStack} from '../lib/some-lib-stack';
+import {SomeStack} from '../some-stack';
 
 const app = new cdk.App();
-new SomeLibStack(app, 'SomestackStack');
+new SomeLibStack(app, 'SomeLibStack');
+new SomeStack(app, 'SomeStack');
